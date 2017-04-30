7c150811786a97cd68158a218218af7a420dba3e
Cristian  R <kilroy05@gmail.com>
2017-04-30 14:08:41 +0100

    Add material design. Create config Service.

---
+++ src/app/services/config/config-service.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ConfigServiceService {
+
+  //add configuration settings
+  whenHubApiKey: string = 'bmPLjpJyIZHJpd7PzLPDouYmwA4dy9HPgKfiyAsRCzTMy3EoNvebKCOYTUy3diPL';
+
+  constructor() { }
+
+}
